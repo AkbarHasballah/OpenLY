91c161757474874a0250a097b93a565c2d479a2d
Salman Akbar Hasbullah <salmanakbartafisa@gmail.com>
2023-11-13 13:57:26 +0700

    TESTING

--- SIG/js/template/template.js
+++ SIG/js/template/template.js
@@ -1,5 +1,5 @@
-export let URLGeoJson = "https://us-central1-bustling-walker-340203.cloudfunctions.net/function-4";
-export let urlPostGCF = "https://us-central1-bustling-walker-340203.cloudfunctions.net/function-4";
+export let URLGeoJson = "https://us-central1-bustling-walker-340203.cloudfunctions.net/function-2";
+export let urlPostGCF = "https://us-central1-bustling-walker-340203.cloudfunctions.net/function-2";
 export let tableTag="tr";
 export let tableRowClass="content is-small";
 export let tableTemplate=`
